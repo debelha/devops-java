09d9dbc4d5d212fd952bf41c3975ef475211d09a
debelha <dleitedias@gmail.com>
2025-04-02 17:40:40 -0300

    ajuste workflows

--- src/main/java/com/exemplo/HelloWorld.java
+++ src/main/java/com/exemplo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.exemplo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("\n\n Hello, DevOps!");
+        System.out.println("\n\n Hello, DevOps world!");
     }
 }
